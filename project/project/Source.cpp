d7ac47448855bd37a63f3737ec59e73f4f6eddec
Tran Tan Phat <phat21212121@gmail.com>
2020-10-29 16:05:36 +0700

    randomtru

--- project/project/Source.cpp
+++ project/project/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 int tong(int a, int b);
@@ -22,6 +23,7 @@ int main()
 	cout << "Hieu la: " << hieu(6, 5);
 	cout << "Tich la: " << tich(6, 5);
 	cout << "Thuong la: " << thuong(6, 5);
+	cout<<"Random tru la"<<random;
 	system("pause");
 	return 0;
 }
@@ -34,3 +36,9 @@ int tong(int a, int b)
 int tich(int a, int b) {
 	return a * b;
 }
+int randomtru()
+{
+	int kq=0;
+	return kq-=rand();
+}
+	
